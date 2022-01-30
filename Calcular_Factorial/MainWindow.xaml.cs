f906343d60595a4850ec45e7e31247279625013b
Sander <sanderpeguero.21@gmail.com>
2022-01-30 15:46:09 -0400

    Mac

---
+++ Calcular_Factorial/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calcular_Factorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Calcular(object sender, RoutedEventArgs e){
+            int numero = int.Parse(NumeroTextBox.Text);
+            MessageBox.Show("Factorial: " + Factorial(numero));
+        }
+
+        int Factorial(int numero)
+        {
+
+            if (numero == 0)
+            {
+
+                return 1;
+
+            }
+            else
+            {
+
+                return numero * Factorial(numero - 1);
+            }
+        }
+    }
+}
---
+++ Entero_a_Cadena/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Entero_a_Cadena
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Convertir(object sender, EventArgs e){
+            int n = int.Parse(Numero.Text);
+            MessageBox.Show("Resultado: " + FuncionCadena(n));
+        }
+        string FuncionCadena(int n)
+        {           
+            return n.ToString();
+
+        }
+    }
+}
